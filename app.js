8df6a8fdc0dcbc29d30bcdc5ae850c8c62e63439
akashzone <akashnadar.dev@gmail.com>
2026-02-11 21:34:11 +0530

    mongoose connection setup complete

--- app.js
+++ app.js
@@ -2,7 +2,17 @@ const express = require("express");
 const app = express();
 
 const mongoose = require("mongoose");
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
+async function main() {
+    await mongoose.connect(MONGO_URL);    
+}
+
+main().then(()=>{
+    console.log("Yess! Connected to DB!");
+}).catch(err =>
+    console.log(err)
+)
 
 app.get("/",(req,res)=>{
     res.send("Yess, working on root");

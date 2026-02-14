20ef7e3f973457e302903691b6b0a7fd46f5dfef
akashzone <akashnadar.dev@gmail.com>
2026-02-14 22:17:29 +0530

    create navbar.ejs & use ejs-mate package

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ const path = require("path");
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static(path.join(__dirname, "public")));
+
+const ejsMate = require("ejs-mate");
+app.engine("ejs", ejsMate);
 
 const methodOverride = require("method-override");
 app.use(methodOverride("_method"));
